116572ce6ddb31f7b8b62cb714c591ef0a877b4b
stark <80265414+shaN480@users.noreply.github.com>
2023-12-12 17:53:16 +0530

    Create log3.sh

---
+++ img/log3.sh
@@ -0,0 +1,68 @@
+Content
+{
+  "RoleLocation": "East US 2",
+  "ReleaseVersion": "6.2023.45.7+4eb0660.release_2023w45",
+  "time": "2023-12-12T12:06:20.0559464Z",
+  "resourceId": "/SUBSCRIPTIONS/69349ACF-2A6C-409D-A925-96BA0C5F099F/RESOURCEGROUPS/DEFAULTRESOURCEGROUP-EUS",
+  "operationName": "MICROSOFT.RESOURCES/SUBSCRIPTIONS/RESOURCEGROUPS/DELETE",
+  "category": "Administrative",
+  "resultType": "Success",
+  "resultSignature": "Succeeded.",
+  "durationMs": "0",
+  "callerIpAddress": "49.207.199.30",
+  "correlationId": "0c808bf1-beec-4a23-88d7-ed1a5adcbee6",
+  "identity": {
+    "authorization": {
+      "scope": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f/resourceGroups/DefaultResourceGroup-EUS",
+      "action": "Microsoft.Resources/subscriptions/resourceGroups/delete",
+      "evidence": {
+        "role": "Contributor",
+        "roleAssignmentScope": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f",
+        "roleAssignmentId": "95a6e1077f8c434d9400106e7d9ba68c",
+        "roleDefinitionId": "b24988ac618042a0ab8820f7382dd24c",
+        "principalId": "f7202482ee084d1c86651b25942e1c59",
+        "principalType": "User"
+      }
+    },
+    "claims": {
+      "aud": "https://management.core.windows.net/",
+      "iss": "https://sts.windows.net/4fcfc298-3c41-4567-89b7-982496662939/",
+      "iat": "1702379433",
+      "nbf": "1702379433",
+      "exp": "1702384525",
+      "http://schemas.microsoft.com/claims/authnclassreference": "1",
+      "aio": "AVQAq/8VAAAAjqDKcDgQjHhBNX8FklW/Q4r6zOhJ4fAe2em+vOsANAvK+EXohb3behT+csHq9K4PMBH2MuIznNr6LnAxCS1eLIdpKsK+Ry2oSGgH/nHCrzQ=",
+      "http://schemas.microsoft.com/claims/authnmethodsreferences": "pwd,mfa",
+      "appid": "c44b4083-3bb0-49c1-b47d-974e53cbdf3c",
+      "appidacr": "0",
+      "groups": "31c1e112-4780-483c-8e2e-c1fdc4258c7e,f59ef32c-5220-4681-bc9d-6ce4379285bc,69d59e38-a45a-4669-b2a1-943e98f097bd,e17f614e-a80e-4997-83c9-dd0804aff4cd,f1a83364-707e-4522-985d-1affb61aa00e,1b7abaa9-7088-4d16-9de4-2d9e00db798e,0a0760d6-cdbf-482f-af74-11599fde66de,242b79df-d8e3-41ea-a084-8fb75690b5ce,85670cf6-a5f6-48be-a980-24cd382e9cdb",
+      "idtyp": "user",
+      "ipaddr": "2406:7400:56:6a4b:5437:e2c2:7da9:93ad",
+      "name": "user",
+      "http://schemas.microsoft.com/identity/claims/objectidentifier": "f7202482-ee08-4d1c-8665-1b25942e1c59",
+      "puid": "10032002F9C78227",
+      "rh": "0.AVYAmMLPT0E8Z0WJt5gklmYpOUZIf3kAutdPukPawfj2MBOfABY.",
+      "http://schemas.microsoft.com/identity/claims/scope": "user_impersonation",
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": "JObOiML3N8F8kmgR_C6SWukLJiCe7NdwPt1JyKdMVio",
+      "http://schemas.microsoft.com/identity/claims/tenantid": "4fcfc298-3c41-4567-89b7-982496662939",
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": "user@gauravkcloudjournee.onmicrosoft.com",
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn": "user@gauravkcloudjournee.onmicrosoft.com",
+      "uti": "b1d2WtcbMEWnPgvXMWN6AA",
+      "ver": "1.0",
+      "wids": "58a13ea3-c632-46ae-9ee0-9c0d43cd7f3d,cf1c38e5-3621-4004-a7cb-879624dced7c,9c6df0f2-1e7c-4dc3-b195-66dfbd24aa8f,8424c6f0-a189-499e-bbd0-26c1753c96d4,c430b396-e693-46cc-96f3-db01bf8bb62a,ffd52fa5-98dc-465c-991d-fc073eb59f8f,1d336d2c-4ae8-42ef-9711-b3604ce3fc2c,9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3,c4e39bd9-1100-46d3-8c65-fb160da0071f",
+      "xms_cae": "1",
+      "xms_tcdt": "1671088307"
+    }
+  },
+  "level": "Information",
+  "properties": {
+    "statusCode": "OK",
+    "eventCategory": "Administrative",
+    "entity": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f/resourcegroups/DefaultResourceGroup-EUS",
+    "message": "Microsoft.Resources/subscriptions/resourcegroups/delete",
+    "hierarchy": "69349acf-2a6c-409d-a925-96ba0c5f099f"
+  },
+  "tenantId": "4fcfc298-3c41-4567-89b7-982496662939",
+  "jobId": "RESOURCEGROUPDELETIONJOB-DEFAULTRESOURCEGROUP:2DEUS-EASTUS",
+  "jobType": "ResourceGroupDeletionJob"
+}
