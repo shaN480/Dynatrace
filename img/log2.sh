46bedafdd3d8daf9653cacc7913bf0e3d5c05697
stark <80265414+shaN480@users.noreply.github.com>
2023-12-12 17:51:21 +0530

    Create log2.sh

---
+++ img/log2.sh
@@ -0,0 +1,34 @@
+Content
+{
+  "count": 5,
+  "total": 500,
+  "minimum": 100,
+  "maximum": 100,
+  "resourceId": "/SUBSCRIPTIONS/69349ACF-2A6C-409D-A925-96BA0C5F099F/RESOURCEGROUPS/RESOURCE/PROVIDERS/MICROSOFT.STORAGE/STORAGEACCOUNTS/FUNCTIONDYNATRACESA4NI6",
+  "time": "2023-12-12T12:11:00.0000000Z",
+  "metricName": "Availability",
+  "timeGrain": "PT1M",
+  "AccountResourceId": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f/resourceGroups/resource/providers/Microsoft.Storage/storageAccounts/functiondynatracesa4ni6",
+  "ApiName": "RenewBlobLease",
+  "Authentication": "AccountKey",
+  "GeoType": "Primary",
+  "average": 100
+}
+
+
+Content
+{
+  "count": 10,
+  "total": 1000,
+  "minimum": 100,
+  "maximum": 100,
+  "resourceId": "/SUBSCRIPTIONS/69349ACF-2A6C-409D-A925-96BA0C5F099F/RESOURCEGROUPS/RESOURCE/PROVIDERS/MICROSOFT.STORAGE/STORAGEACCOUNTS/FUNCTIONDYNATRACESA4NI6",
+  "time": "2023-12-12T12:11:00.0000000Z",
+  "metricName": "Availability",
+  "timeGrain": "PT1M",
+  "AccountResourceId": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f/resourceGroups/resource/providers/Microsoft.Storage/storageAccounts/functiondynatracesa4ni6",
+  "ApiName": "SetBlobMetadata",
+  "Authentication": "AccountKey",
+  "GeoType": "Primary",
+  "average": 100
+}
