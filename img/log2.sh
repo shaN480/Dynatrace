6b182b4b95c2c0d8af51c48289dd0b101b21e86d
stark <80265414+shaN480@users.noreply.github.com>
2023-12-12 17:52:15 +0530

    Update log2.sh

--- img/log2.sh
+++ img/log2.sh
@@ -32,3 +32,21 @@ Content
   "GeoType": "Primary",
   "average": 100
 }
+
+
+Content
+{
+  "count": 9,
+  "total": 57,
+  "minimum": 5,
+  "maximum": 8,
+  "resourceId": "/SUBSCRIPTIONS/69349ACF-2A6C-409D-A925-96BA0C5F099F/RESOURCEGROUPS/RESOURCE/PROVIDERS/MICROSOFT.STORAGE/STORAGEACCOUNTS/FUNCTIONDYNATRACESA4NI6",
+  "time": "2023-12-12T12:07:00.0000000Z",
+  "metricName": "SuccessServerLatency",
+  "timeGrain": "PT1M",
+  "AccountResourceId": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f/resourceGroups/resource/providers/Microsoft.Storage/storageAccounts/functiondynatracesa4ni6",
+  "ApiName": "SetBlobMetadata",
+  "Authentication": "AccountKey",
+  "GeoType": "Primary",
+  "average": 6.33333333333333
+}
