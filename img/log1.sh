4017459f395fdbb75b235b3ff614bc904affe7b1
stark <80265414+shaN480@users.noreply.github.com>
2023-12-12 17:49:50 +0530

    Create log1.sh

---
+++ img/log1.sh
@@ -0,0 +1,36 @@
+Content
+{
+  "time": "2023-12-12T12:12:19.1357605Z",
+  "resourceId": "/subscriptions/69349acf-2a6c-409d-a925-96ba0c5f099f/resourceGroups/resource/providers/Microsoft.Storage/storageAccounts/functiondynatracesa4ni6/blobServices/default",
+  "category": "StorageWrite",
+  "operationName": "SetBlobMetadata",
+  "operationVersion": "2022-11-02",
+  "schemaVersion": "1.0",
+  "statusCode": 200,
+  "statusText": "Success",
+  "durationMs": 5,
+  "callerIpAddress": "10.50.0.204:38208",
+  "correlationId": "4dfed43b-801e-001e-47f4-2c3e77000000",
+  "identity": {
+    "type": "AccountKey",
+    "tokenHash": "key1(67F8F5A218837386F94E75FC7AAC530CD99957355C86E173D139401AC15D6011)"
+  },
+  "location": "eastus",
+  "properties": {
+    "accountName": "functiondynatracesa4ni6",
+    "userAgentHeader": "azsdk-net-Storage.Blobs/12.16.0 (.NET 6.0.25; Linux 5.15.131.1-2.cm2 #1 SMP Sun Sep 24 03:38:45 UTC 2023)",
+    "clientRequestId": "7691b1b0-58a3-4ed3-86cd-8bd8bec67cac",
+    "etag": "\"0x8DBFB0B96816382\"",
+    "serviceType": "blob",
+    "objectKey": "/functiondynatracesa4ni6/azure-webjobs-eventhub/nippon.servicebus.windows.net/http/$default/checkpoint/0",
+    "lastModifiedTime": "12/12/2023 12:12:19 PM",
+    "metricResponseType": "Success",
+    "serverLatencyMs": 5,
+    "requestHeaderSize": 648,
+    "responseHeaderSize": 272,
+    "tlsVersion": "TLS 1.2"
+  },
+  "uri": "https://functiondynatracesa4ni6.blob.core.windows.net:443/azure-webjobs-eventhub/nippon.servicebus.windows.net/http/%24default/checkpoint/0?comp=metadata",
+  "protocol": "HTTPS",
+  "resourceType": "Microsoft.Storage/storageAccounts/blobServices"
+}
